0c52aa3043777f563c22c0ede1673d98aa4b4d45
Arth Raval <connect.arth.raval@gmail.com>
2025-11-23 10:56:31 +0530

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div className="font-game">
+
+      <p>welcome to codebox</p>
+      <Button className="m-4 font-game">Subscribe</Button>
+    </div>
+  );
+}
